70f78e4857797951c7d3b2be778b8f9509476941
RuliLG <RuliLG@users.noreply.github.com>
2022-09-19 08:16:14 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,11 @@
 
 namespace Calima\FilamentPageBuilder\Tests;
 
+use Calima\FilamentPageBuilder\FilamentPageBuilderServiceProvider;
 use Filament\FilamentServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Calima\FilamentPageBuilder\FilamentPageBuilderServiceProvider;
 
 class TestCase extends Orchestra
 {
